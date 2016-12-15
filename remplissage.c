5e217896247608c8b89e19b55fd415b8024b4817
Shaeli <eleonore.carpentier@insa-cvl.fr>
2016-12-15 18:33:24 +0100

    MAJ Ele

--- remplissage.c
+++ remplissage.c
@@ -20,7 +20,6 @@ void recuperation_donnees(FILE *fp, int nb_lignes)
         char l[20];
         char lo[20];
         time_t test_date;
-        struct tm temps;
         long double test_lat;
         long double test_long;
         fseek(fp,0,SEEK_SET);
@@ -104,8 +103,7 @@ void recuperation_donnees(FILE *fp, int nb_lignes)
             {
             logGlobal.tableauPoint[nb_donnees].longitude=test_long;
             logGlobal.tableauPoint[nb_donnees].latitude=test_lat;
-            temps=*localtime(&test_date);
-            logGlobal.tableauPoint[nb_donnees].date=temps;
+            logGlobal.tableauPoint[nb_donnees].date=test_date;
             nb_donnees++;
 
 
@@ -121,16 +119,18 @@ void recuperation_donnees(FILE *fp, int nb_lignes)
 
 
 
-void CopiePoints (point * point1,point * point2)
+
+void CopiePoints (point* point1, point* point2)
 {
+
     *point2=*point1;
+
 }
 
-logs CopieTableau (logs tlog,int taille)
+logs CopieTableau(logs tlog,int taille)
 {
     logs tableauCopie=AllocationTableauPoint(taille);
     int i;
-    tableauCopie.tailleTab=taille;
     for (i=0;i<taille;i++)
     {
         CopiePoints(&tlog.tableauPoint[i],&tableauCopie.tableauPoint[i]);
@@ -142,14 +142,24 @@ logs CopieTableau (logs tlog,int taille)
 
 void BackupFile (logs tlog)
 {
+
+    static int nb_passages=0;
     FILE * fichier;
     int i;
-    fichier=fopen("BackupPoints.txt","w+");
-    fseek(fichier,0,SEEK_END);
+    if(nb_passages==0)
+    {
+        fichier=fopen("BackupPoints.txt","w+");
+    }
+    else
+    {
+        fichier=fopen("BackupPoints.txt","a");
+        fseek(fichier,0,SEEK_END);
+    }
     for(i=0;i<tlog.tailleTab;i++)
     {
+      
         fputs("date:",fichier);
-        fprintf(fichier,"%time_t",mktime(&tlog.tableauPoint[i].date));
+        fprintf(fichier,"%ld",tlog.tableauPoint[i].date);
         fputs(",lat:",fichier);
         fprintf(fichier,"%Lf",tlog.tableauPoint[i].latitude);
         fputs(",long:",fichier);
@@ -157,5 +167,72 @@ void BackupFile (logs tlog)
         fputs("\n",fichier);
     }
     fclose(fichier);
+    nb_passages++;
+}
 
+logs resurrection_point(char * nom)
+{
+        FILE *fp;
+        fp=fopen(nom,"r");
+        int nb_lignes;
+        nb_lignes--;
+        nb_lignes=Nombre_lignes(fp);
+        int i;
+        time_t date;
+        char c[20];
+        long double lat;
+        long double longi;
+        fseek(fp,0,SEEK_SET);
+        logs logBack = AllocationTableauPoint(nb_lignes);
+        for(i=0;i<nb_lignes;i++)
+        {
+            fgets(c,6,fp);
+            fscanf(fp,"%ld",&date);
+            fgets(c,6,fp);
+            fscanf(fp,"%Lf",&lat);
+            fgets(c,7,fp);
+            fscanf(fp,"%Lf",&longi);
+            fgets(c,2,fp);
+            logBack.tableauPoint[i].longitude=longi;
+            logBack.tableauPoint[i].latitude=lat;
+            logBack.tableauPoint[i].date=date;
+
+            
+        }
+ 
+
+        logBack.tailleTab=nb_lignes;
+        return logBack;
+
+}
+
+addr recuperation_addr()
+{
+    addr addrrecup;
+    FILE *fp;
+    char c;
+    int i,j,nb_lignes;
+    fp=fopen("AddrConvert.txt","r");
+    
+    nb_lignes=Nombre_lignes(fp);
+    fseek(fp,0,SEEK_SET);
+    addrrecup.tailleTableauAddr=nb_lignes;
+    addrrecup.tableauPointaddr=(pointaddr*)malloc(nb_lignes*sizeof(pointaddr));
+    for(i=0;i<nb_lignes;i++)
+    {
+        for(j=0;j<4;j++)
+        {
+            c=fgetc(fp);
+            while(c !=',')
+            {
+                c=fgetc(fp);
+            }
+        }
+        fscanf(fp,"%Lf",&(addrrecup.tableauPointaddr[i].latitude));
+        c=fgetc(fp);
+        fscanf(fp,"%Lf",&(addrrecup.tableauPointaddr[i].longitude));
+        c=fgetc(fp);
+
+    }
+    return addrrecup;
 }
